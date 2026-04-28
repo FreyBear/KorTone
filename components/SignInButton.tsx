50dab46c3a56f5c0e1b9b49b26b3fe939be886da
Ingve Bjørnå <57219049+FreyBear@users.noreply.github.com>
2026-04-28 20:06:52 +0000

    Use static callback path for one.com export

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -19,7 +19,7 @@ export default function SignInButton() {
       await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/auth/callback.html`,
           queryParams: {
             apikey: supabaseKey ?? '',
           },
